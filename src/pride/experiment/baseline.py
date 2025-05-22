fe460e866e6db671bded0fabbc43b4e504d55074
Alfonso <86886965+alfonsoSR@users.noreply.github.com>
2025-05-22 22:54:20 +0200

    Remove functionality to agument data from baseline

--- src/pride/experiment/baseline.py
+++ src/pride/experiment/baseline.py
@@ -19,32 +19,17 @@ class Baseline:
     :param center: Station object representing the phase center
     :param station: Station object representing the other station in the baseline
     :param observations: List of observations associated with the baseline
-    :param nobs: Number of observations associated with the baseline
     :param tstamps: Time stamps of the observations associated with the baseline
-    :param exp: Experiment object to which the baseline belongs
+    :param id: Unique identifier for the baseline (center-station)
     """
 
-    # __slots__ = (
-    #     "center",
-    #     "station",
-    #     "observations",
-    #     "tstamps",
-    #     "a_tstamps",
-    #     "a_eops",
-    #     "icrf2itrf",
-    #     "a_icrf2itrf",
-    #     "dot_icrf2itrf",
-    #     "a_seu2itrf",
-    #     "a_lat",
-    #     "a_lon",
-    # )
+    __slots__ = ("center", "station", "observations", "tstamps", "id")
 
     def __init__(
         self,
         center: "Station",
         station: "Station",
         observations: list["Observation"],
-        eops: "coord.EOP",
     ) -> None:
 
         # Merge timestamps of all the observations
@@ -59,85 +44,26 @@ class Baseline:
             location=station.tectonic_corrected_location(tstamps_no_location),
         )
 
-        # Augment time stamps with +/- 1 second around each epoch
-        __augmented_tstamps: time.Time = (
-            tstamps_no_location[:, None]
-            + time.TimeDelta([-1, 0, 1], format="sec")[None, :]
-        ).ravel()  # type: ignore
-        a_tstamps = time.Time(
-            __augmented_tstamps,
-            scale="utc",
-            location=station.tectonic_corrected_location(__augmented_tstamps),
-        )
-        assert a_tstamps.location is not None
-
-        # Calculate geodetic coordinates of station
-        a_geodetic = a_tstamps.location.to_geodetic("GRS80")
-        a_lat = np.array(a_geodetic.lat.rad, dtype=float)
-        a_lon = np.array(a_geodetic.lon.rad, dtype=float)
-
-        # Calculate rotation matrices
-        augmented_eops = eops.at_epoch(a_tstamps, unit="arcsec")
-        # self.a_eops = eops.at_epoch(self.a_tstamps, unit="arcsec")
-        a_icrf2itrf = coord.icrf2itrf(augmented_eops, a_tstamps)
-        a_seu2itrf = coord.seu2itrf(a_lat, a_lon)
-
-        # Calculate derivative of ICRF to ITRF rotation matrix
-        dt_tdb: np.ndarray = (
-            (a_tstamps.tdb[2::3] - a_tstamps.tdb[0::3])
-            .to("s")  # type: ignore
-            .value
-        )
-        diff_icrf2itrf = a_icrf2itrf[2::3] - a_icrf2itrf[0::3]
-        dot_icrf2itrf = diff_icrf2itrf / dt_tdb[:, None, None]
-
-        # Get attributes at observation epochs
-        icrf2itrf = a_icrf2itrf[1::3]
-        seu2itrf = a_seu2itrf[1::3]
-
-        # Update observations with calculated data
-        for observation in observations:
-
-            # Get the index of the time stamps associated with the observation
-            flags = np.sum(
-                observation.tstamps[:, None] == tstamps[None, :],
-                axis=0,
-                dtype=bool,
-            )
-
-            observation.icrf2itrf = icrf2itrf[flags]
-            observation.seu2itrf = seu2itrf[flags]
-            observation.dot_icrf2itrf = dot_icrf2itrf[flags]
-
         # Attributes
         self.center = center
         self.station = station
         self.observations = observations
         self.tstamps = tstamps
-        self.a_tstamps = a_tstamps
-        self.a_eops = augmented_eops
-        self.icrf2itrf = icrf2itrf
-        self.a_icrf2itrf = a_icrf2itrf
-        self.dot_icrf2itrf = dot_icrf2itrf
-        self.a_seu2itrf = a_seu2itrf
-        self.a_lat = a_lat
-        self.a_lon = a_lon
+        self.id = f"{center.name}-{station.name}"
 
         return None
 
-    @property
-    def id(self) -> str:
-        return f"{self.center.name}-{self.station.name}"
-
-    @property
-    def nobs(self) -> int:
-        return len(self.observations)
-
-    def __str__(self) -> str:
-        return self.id
-
     def update_station_with_geophysical_displacements(
-        self, displacement_models: list["Displacement"]
+        self,
+        displacement_models: list["Displacement"],
+        augmented_tstamps: time.Time,
+        augmented_eops: np.ndarray,
+        augmented_icrf2itrf: np.ndarray,
+        augmented_seu2itrf: np.ndarray,
+        augmented_lat: np.ndarray,
+        augmented_lon: np.ndarray,
+        icrf2itrf: np.ndarray,
+        dot_icrf2itrf: np.ndarray,
     ) -> None:
 
         log.debug(
@@ -148,35 +74,37 @@ class Baseline:
         # Shared resources
         shared_resources = {
             "station_names": self.station.possible_names,
-            "eops": self.a_eops,
-            "icrf2itrf": self.a_icrf2itrf,
-            "seu2itrf": self.a_seu2itrf,
-            "lat": self.a_lat,
-            "lon": self.a_lon,
-            "xsta_itrf": self.station.location(self.a_tstamps),
+            "eops": augmented_eops,
+            "icrf2itrf": augmented_icrf2itrf,
+            "seu2itrf": augmented_seu2itrf,
+            "lat": augmented_lat,
+            "lon": augmented_lon,
+            "xsta_itrf": self.station.location(augmented_tstamps),
         }
         log.debug("Right after shared resources")
 
         # Station position at reference epochs [Tectonic corrected]
         xsta_itrf = shared_resources["xsta_itrf"][1::3]
         xsta_icrf = (
-            self.icrf2itrf.swapaxes(-1, -2) @ xsta_itrf[:, :, None]
+            icrf2itrf.swapaxes(-1, -2) @ xsta_itrf[:, :, None]
         ).squeeze()
 
         # Station velocity at reference epochs
         vsta_itrf = np.zeros_like(xsta_itrf)
         vsta_icrf = (
-            self.dot_icrf2itrf.swapaxes(-1, -2) @ xsta_itrf[:, :, None]
+            dot_icrf2itrf.swapaxes(-1, -2) @ xsta_itrf[:, :, None]
         ).squeeze()
 
         # Update positions and velocities with displacements
         for model in displacement_models:
 
             # Calculate augmented displacement in ICRF and ITRF
-            resources = model.load_resources(self.a_tstamps, shared_resources)
-            a_dx_itrf = model.calculate(self.a_tstamps, resources)
+            resources = model.load_resources(
+                augmented_tstamps, shared_resources
+            )
+            a_dx_itrf = model.calculate(augmented_tstamps, resources)
             a_dx_icrf = (
-                self.a_icrf2itrf.swapaxes(-1, -2) @ a_dx_itrf[:, :, None]
+                augmented_icrf2itrf.swapaxes(-1, -2) @ a_dx_itrf[:, :, None]
             ).squeeze()
 
             # Update positions and velocities
