dd8a0bd3b5f5b8de7e7dcc0d4060bfe813f1a3d4
Alfonso <86886965+alfonsoSR@users.noreply.github.com>
2025-06-12 15:03:48 +0200

    WIP: Defined test for expected output

--- tests/test_io/test_ionex.py
+++ tests/test_io/test_ionex.py
@@ -11,6 +11,10 @@ from pathlib import Path
 from pride.logger import log
 import os
 from scipy import interpolate
+import numpy as np
+
+# Path to directory with test data
+DATA_DIRECTORY: Path = Path(__file__).parent.parent / "data"
 
 
 @pytest.mark.parametrize(
@@ -103,56 +107,131 @@ def test_download_and_decompress(
 
 @pytest.mark.parametrize(
     [
-        "epoch",
-        "expected_radius",
-        "expected_height",
-        "expected_tec_maps",
-        "content_validation_tuple",
+        "ionex_file",
+        "expected_height_and_rearth",
+        "expected_number_of_elements",
+        "expected_start_and_end",
+        "expected_epoch_lat_lon_tec_combinations",
     ],
     [
         (
-            time.Time("2022-11-27T00:00:00", scale="utc"),
-            6371.0,
-            450.0,
+            "ionex_files/new_ionex.INX",
+            (450.0, 6371.0),
             13,
-            (-175.0, 87.5, 82),
+            (
+                time.Time("2023-10-19T00:00:00"),
+                time.Time("2023-10-20T00:00:00"),
+            ),
+            [(time.Time("2023-10-19T02:00:00"), 75.0, -160, 275)],
         ),
         (
-            time.Time("2022-11-26T00:00:00", scale="utc"),
-            6371.0,
-            450.0,
+            "ionex_files/old_ionex.13i",
+            (450.0, 6371.0),
             13,
-            (-175.0, 87.5, 69),
+            (
+                time.Time("2013-12-28T00:00:00"),
+                time.Time("2013-12-29T00:00:00"),
+            ),
+            [(time.Time("2013-12-28T06:00:00"), -25.0, 150, 527)],
         ),
     ],
-    ids=[
-        "New format",
-        "Old format",
-    ],
 )
-def test_ionex_interface(
-    epoch: "time.Time",
-    expected_height: float,
-    expected_radius: float,
-    expected_tec_maps: int,
-    content_validation_tuple: tuple[float, float, int],
-    tmp_path: Path,
+def test_read_tec_maps(
+    ionex_file: str,
+    expected_height_and_rearth: tuple[float, float],
+    expected_number_of_elements: int,
+    expected_start_and_end: tuple[time.Time, time.Time],
+    expected_epoch_lat_lon_tec_combinations: list[
+        tuple[time.Time, float, float, float]
+    ],
 ) -> None:
 
-    # Initialize interface for file
-    ionex_file = download_ionex_file_for_date(epoch, tmp_path)
-    ionex = IonexInterface(ionex_file)
+    # Initialize ionex interface for chosen file
+    ionex_path = DATA_DIRECTORY / ionex_file
+    ionex = IonexInterface(ionex_path)
 
+    # Read data from file
     tec_maps, ref_height, ref_rearth = ionex.read_data_from_ionex_file()
-    assert isinstance(tec_maps, list)
-    assert isinstance(tec_maps[0], interpolate.RegularGridInterpolator)
-    assert isinstance(ref_height, float)
-    assert isinstance(ref_rearth, float)
-    assert ref_rearth == expected_radius
-    assert ref_height == expected_height
-    assert len(tec_maps) == expected_tec_maps
 
-    lon, lat, expected_tec = content_validation_tuple
-    assert tec_maps[0]([lon, lat]) == expected_tec
+    # Check expected height and Earth radius
+    assert (ref_height, ref_rearth) == expected_height_and_rearth
+
+    # Check it is a dictionary with time.Time keys and interpolators as values
+    assert isinstance(tec_maps, dict)
+    for key, val in tec_maps.items():
+        assert isinstance(key, time.Time)
+        assert isinstance(val, interpolate.RegularGridInterpolator)
+
+    # Ensure that the dictionary has the expected number of elements
+    assert len(tec_maps.keys()) == expected_number_of_elements
+
+    # Check initial and final epochs
+    tec_epochs = list(tec_maps.keys())
+    assert tec_epochs[0] == expected_start_and_end[0]
+    assert tec_epochs[-1] == expected_start_and_end[1]
+
+    # Check some TEC values
+    for epoch, lat, lon, tec in expected_epoch_lat_lon_tec_combinations:
+        assert tec_maps[epoch]([lon, lat]) == tec
 
     return None
+
+
+# @pytest.mark.parametrize(
+#     [
+#         "epoch",
+#         "expected_radius",
+#         "expected_height",
+#         "expected_tec_maps",
+#         "content_validation_tuple",
+#     ],
+#     [
+#         (
+#             time.Time("2022-11-27T00:00:00", scale="utc"),
+#             6371.0,
+#             450.0,
+#             13,
+#             (-175.0, 87.5, 82),
+#         ),
+#         (
+#             time.Time("2022-11-26T00:00:00", scale="utc"),
+#             6371.0,
+#             450.0,
+#             13,
+#             (-175.0, 87.5, 69),
+#         ),
+#     ],
+#     ids=[
+#         "New format",
+#         "Old format",
+#     ],
+# )
+# def test_ionex_interface(
+#     epoch: "time.Time",
+#     expected_height: float,
+#     expected_radius: float,
+#     expected_tec_maps: int,
+#     content_validation_tuple: tuple[float, float, int],
+#     tmp_path: Path,
+# ) -> None:
+
+#     # Directory with IONEX files from tests/data
+#     ionex_path = DATA_DIRECTORY / ionex_file
+
+#     # Initialize interface for file
+#     ionex_file = download_ionex_file_for_date(epoch, tmp_path)
+#     ionex = IonexInterface(ionex_file)
+
+#     tec_maps, ref_height, ref_rearth = ionex.read_data_from_ionex_file()
+#     assert isinstance(tec_maps, list)
+#     assert isinstance(tec_maps[0], interpolate.RegularGridInterpolator)
+#     assert isinstance(ref_height, float)
+#     assert isinstance(ref_rearth, float)
+#     assert ref_rearth == expected_radius
+#     assert ref_height == expected_height
+#     assert len(tec_maps) == expected_tec_maps
+
+#     lon, lat, expected_tec = content_validation_tuple
+#     assert tec_maps[0]([lon, lat]) == expected_tec
+
+#     return None
